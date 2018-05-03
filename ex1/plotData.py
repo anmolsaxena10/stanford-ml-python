dab97abe07a02b32f895d3c06974556474f7f84b
Anmol <anmol.saxena10@gmail.com>
2018-05-03 19:09:08 +0530

    plotData corrected

--- ex1/plotData.py
+++ ex1/plotData.py
@@ -2,6 +2,5 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#data = open('ex1data1.txt', 'r').read()
-#print(data.split('\n'))
-pd.read_csv('ex1data1.txt').plot()
+pd.read_csv('ex1data1.txt').plot(x='Profit', y='Population', style='rx')
+plt.show()
